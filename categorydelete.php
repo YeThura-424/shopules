9d8746fc9241332b8d6a6e9594bc4185aef1884e
YeThura-424 <rathanpoudle.26@gmail.com>
2020-08-06 22:35:03 +0630

    category list page, category delete and brand

--- categorydelete.php
+++ categorydelete.php
@@ -1,12 +1,10 @@
-<?php 
+<?php
 require 'dbconnect.php';
 $id = $_GET['id'];
 
 $sql = "DELETE FROM categories WHERE id=:id";
 $stmt = $conn->prepare($sql);
-$stmt->bindParam(':id',$id);
-$stmt ->execute();
+$stmt->bindParam(':id', $id);
+$stmt->execute();
 
 header('location:categorylist.php');
-
- ?>
