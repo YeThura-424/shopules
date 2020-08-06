f833147ae41e08521025667c8fd69cfc4e22ca30
YeThura-424 <rathanpoudle.26@gmail.com>
2020-08-06 22:31:04 +0630

    first php project

--- home.php
+++ home.php
@@ -1,19 +1,14 @@
 
-<?php 
-
-include("Frontend_Header.php");
-
- ?>
+<?php  
+include("Frontend_Header.php"); 
+ 
+ ?> 
  <?php 
-
-include("Frontend_Nav.php");
-
+include("Frontend_Nav.php"); 
  ?>
 <h1>I AM HOME</h1>
-
 <?php 
-
-    include("Frontend_Footer.php");
-     ?>
+include("Frontend_Footer.php");
+?>
 
 
